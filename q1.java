e1645948a8f3111b6ea9430b2ef3dac0f51a42c6
monika0208 <mohindroomonika@gmail.com>
2025-09-11 09:57:47 +0530

    for and while loop

---
+++ q1.java
@@ -0,0 +1,22 @@
+//for loop
+
+// public class q1{
+//     public static void main(String args[]){
+//         for(int i=1;i<=10;i++){
+//             System.out.print(i+ " ");
+//         }
+//     }
+// }
+
+
+//while loop
+
+public class q1{
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
